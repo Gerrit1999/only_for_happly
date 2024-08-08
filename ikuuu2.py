fe40098ed3c031f90408429c2420b2d11b22a081
Gerrit <gerrit1999@qq.com>
2024-08-08 14:17:51 +0800

    chore: IKuuu机场签到使用notify推送

--- ikuuu2.py
+++ ikuuu2.py
@@ -7,12 +7,12 @@
 # cron "30 5 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('IKuuu机场签到帐号版')
 
-import requests,re
-
-
+import sys
+import re
 import requests
 import os
 import notify
+
 
 # export ikuuu='邮箱&密码'      多号#号隔开
 
@@ -26,17 +26,20 @@ def main():
         passwd = i.split('&')[1]
         sign_in(email, passwd)
         r += 1
+
+
 def sign_in(email, passwd):
     try:
-        body = {"email" : email,"passwd" : passwd,}
-        headers = {'user-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'}
+        body = {"email": email, "passwd": passwd, }
+        headers = {
+            'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+        }
         res = requests.get('https://ikuuu.club/', headers=headers)
         url = re.findall('target="_blank">(.*?)</a>', res.text, re.S)
         for i in range(len(url)):
             resp = requests.session()
             resp.post(f'{url[i]}auth/login', headers=headers, data=body)
             ss = resp.post(f'{url[i]}user/checkin').json()
-    #         print(ss)
             if 'msg' in ss:
                 print(ss['msg'])
                 notify.send("IKuuu机场签到", ss['msg'])
@@ -44,6 +47,8 @@ def sign_in(email, passwd):
     except:
         print('请检查帐号配置是否错误')
         notify.send("IKuuu机场签到", '请检查帐号配置是否错误')
+
+
 def ql_env():
     if "ikuuu" in os.environ:
         token_list = os.environ['ikuuu'].split('#')
@@ -55,6 +60,7 @@ def ql_env():
     else:
         print("未添加ikuuu变量")
         sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
